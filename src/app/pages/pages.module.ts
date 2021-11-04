b0886f5b9e65eb234a55fdb7c0009a7b7ea28f5f
Pedro Covarrubias <pedro_covarrubias@hotmail.com>
2021-11-04 15:16:14 -0600

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -20,19 +21,21 @@ import { ProgressComponent } from './progress/progress.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
     FormsModule,
-    ComponentsModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
